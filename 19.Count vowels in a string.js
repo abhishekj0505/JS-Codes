b8b0e763e263dc783de7fa7177b15958a303256d
abhishek jaiswal <abhishek.jaiswal5385@gmail.com>
2025-04-19 11:05:28 +0530

    added new questions

--- 19.Count vowels in a string.js
+++ 19.Count vowels in a string.js
@@ -1,6 +1,10 @@
 const str = 'abhishek'
 
-const vowels = /[aeiou]/gi
+const vowels = /[aeiou]/gi // Regular expression to match vowels (case-insensitive)
+//i is for case-insensitivity, meaning it will match both uppercase and lowercase letters.
+
+
+//The g flag stands for global. It tells the regex engine to find all matches in the string — not just the first one.
 
 const foundVowels = str.match(vowels)
 
